44cac7607e51000e9e845c6fb106271c61ca8eab
yorwils <yorwi_torres_123@hotmail.com>
2022-02-28 13:23:50 -0500

    reto encargado por el profe jesus

---
+++ formappreto01/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace formappreto01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSerie = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblSerie
+            // 
+            this.lblSerie.AutoSize = true;
+            this.lblSerie.Location = new System.Drawing.Point(229, 61);
+            this.lblSerie.Name = "lblSerie";
+            this.lblSerie.Size = new System.Drawing.Size(39, 13);
+            this.lblSerie.TabIndex = 0;
+            this.lblSerie.Text = "SERIE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 284);
+            this.Controls.Add(this.lblSerie);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSerie;
+    }
+}
+
---
+++ formappreto01/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formappreto01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text = "reto1xdxd";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+            
+                lblSerie.Text = lblSerie.Text + "\n" + j;
+
+
+            }
+        }
+    }
+}
